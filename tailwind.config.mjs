c76a4040a02527e84601630f8f25c1442f584549
dELCIo <64657972+dasandza@users.noreply.github.com>
2024-07-28 19:24:47 +0100

    fonts added to index.css

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,23 @@ import lineClamp from '@tailwindcss/line-clamp';
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,tsx,jsx}"],
     theme: {
-        extend: {},
+        extend: {
+            screens: {
+                "tablet": "480px",
+                "laptop": "768px",
+                "desktop": "1024px",
+                "tv": "1200px",
+            },
+            fontFamily: {
+                'poppins': ['Poppins', 'sans-serif'], // Basic definition to use the font across Tailwind classes
+            },
+            fontWeight: {
+                'poppins-regular': '400',
+                'poppins-light': '300',
+                'poppins-medium': '500',
+                'poppins-semibold': '600',
+            }
+        },
     },
     plugins: [
         lineClamp
