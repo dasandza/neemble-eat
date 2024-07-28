dc584a4ec5687b6ba68c6b380f41b93e58451b2c
dELCIo <64657972+dasandza@users.noreply.github.com>
2024-07-28 18:23:03 +0100

    airtable dependency added

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+import {defineConfig} from 'tailwindcss';
+import lineClamp from '@tailwindcss/line-clamp';
+
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,tsx,jsx}"],
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [
+        lineClamp
+    ],
 }
 
