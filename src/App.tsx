4e67d732459264dc3802c3dffa031f641d1aadbc
dELCIo <64657972+dasandza@users.noreply.github.com>
2024-07-28 19:30:29 +0100

    App added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {Menu, Placeholder, Product, Cart, Orders} from './Pages'
+
 function App() {
 
+
     return (
-        <>
-            Hello World
-        </>
+        <div className="w-full overflow-hidden">
+            <Router>
+                <Routes>
+                    <Route path="/b/:encodedPlaceName/:tableNumber"
+                           element={<Menu/>}/>
+                    <Route path="/"
+                           element={<Placeholder/>}/>
+                    <Route path="/p/:encodedBusinessName/:tableNumber/:productId"
+                           element={<Product/>}/>
+                    <Route path='/c/:encodedBusinessName/:tableNumber'
+                           element={<Cart/>}/>
+                    <Route path="/o/:encodedBusinessName/:tableNumber"
+                           element={<Orders/>}/>
+                </Routes>
+            </Router>
+        </div>
     )
 }
 
