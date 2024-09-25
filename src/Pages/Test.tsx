443b68f8bb57f9dc053ab5c97861a676f3a83d10
dELCIo <64657972+dasandza@users.noreply.github.com>
2024-09-25 17:47:23 +0100

    cleared the testing page

--- src/Pages/Test.tsx
+++ src/Pages/Test.tsx
@@ -1,18 +1,9 @@
-import useRestaurantData from "../api/functions/fetchRestaurant.ts";
-
 function Test() {
-
-
-    const restaurantID = "FUHT4zQL5Umz99BN7dUI"
-
-    const {restaurant} = useRestaurantData({restaurantID: restaurantID});
 
 
     return (
         <div className="relative">
-            {
-                restaurant ? restaurant.name : "espera"
-            }
+
         </div>
     );
 }
