fcf9bcb3bd5adec52483a73e90be0f67460571a1
dELCIo <64657972+dasandza@users.noreply.github.com>
2024-09-25 17:40:22 +0100

    this time i think it is fixed

--- src/api/index.ts
+++ src/api/index.ts
@@ -9,7 +9,7 @@ import fetchRepresentant from "./functions/fetchRepresentant.ts";
 import fetchAllRestaurantOrders from "./functions/fetchAllRestaurantOrders.ts";
 import fetchRestaurant from "./functions/fetchRestaurant.ts";
 import fetchRepresentantByUUID from "./functions/fetchRepresentantByUUID.ts";
-import fetchMenuParsed from "./functions/fetchMenuParsed.ts";
+import {fetchMenuParsed} from "./functions/fetchMenuParsed.ts";
 import fetchRestaurantOpenTable from "./functions/fetchRestaurantOpenTable.ts";
 import addOrder from "./functions/addOrder.ts";
 import fetchAllSessionOrders from "./functions/fetchAllSessionOrders.ts";
